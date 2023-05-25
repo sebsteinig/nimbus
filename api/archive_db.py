921668ce2c279d56c74b385ebe264011a4a34e14
willemNicolas <darwil.nicolas@gmail.com>
2023-05-25 18:25:37 +0100

    creation of migrations process

--- api/archive_db.py
+++ api/archive_db.py
@@ -1,14 +1,19 @@
+from datetime import datetime, timedelta
+import json
 from utils.logger import Logger
 from utils.metadata import Metadata
 from dataclasses import dataclass
 from typing import List, Union
 import requests
 from dotenv import dotenv_values
+import os.path as path
+from os import listdir, mkdir, remove
 
 @dataclass
 class ArchiveDB:
     url:str
     experiments : dict
+    commit_dir : str
     
     def add(self,exp_id:str,
             variable_name:str,
@@ -24,6 +29,7 @@ class ArchiveDB:
         table_nimbus_execution_row = {
             "exp_id":exp_id,
             "config_name":config_name,
+            "created_at":(datetime.now() - timedelta(days=10)).isoformat(),
             "extension":extension,
             "lossless":lossless,
             "nan_value_encoding":metadata.general_metadata.nan_value_encoding,
@@ -55,23 +61,53 @@ class ArchiveDB:
             }
     
     def commit(self):
-        #print(self.experiments)
-        
-        pass
+        if not path.isdir(self.commit_dir) :
+            mkdir(self.commit_dir)
+        for exp_id,rows in self.experiments.items():
+            commit_file = path.join(self.commit_dir,f"commit_{exp_id}_{datetime.now().strftime('%d_%m_%Y_%H:%M')}.json")
+            with open(commit_file,"w") as file:
+                file.write(json.dumps({
+                    "date": datetime.now().isoformat(),
+                    "exp_id":exp_id,
+                    "body" : {"request":rows}
+                }))
         
     def push(self) -> bool:
-        for exp_id,rows in self.experiments.items():
-            url = f"{self.url}/experiment/{exp_id}/add"
-            Logger.console().info(url)
-            res = requests.post(url,json={"request":rows})
-            Logger.console().info(res.status_code)
-            Logger.console().info(res.text)
-            
+        if not path.isdir(self.commit_dir) :
+            result = True
+            for exp_id,rows in self.experiments.items():
+                url = f"{self.url}/insert/{exp_id}"
+                try :
+                    res = requests.post(url,json={"request":rows})          
+                    if not res.ok:
+                        result = False
+                except :
+                    result = False
+            return result
+        else :
+            result = True
+            requests_list = []
+            for file in listdir(self.commit_dir):
+                with open(path.join(self.commit_dir,file),'r') as rd :
+                    requests_list.append((path.join(self.commit_dir,file),json.loads(rd.read())))
+            requests_list.sort(key=lambda item:datetime.fromisoformat(item[1]['date']), reverse=True)
+            for file,request in requests_list:
+                url = f"{self.url}/insert/{request['exp_id']}"
+                try :
+                    res = requests.post(url,json=request['body'])
+                    if res.ok:
+                        remove(file)
+                    else :
+                        result = False
+                except :
+                    result = False
+            return result
     @staticmethod
     def build() -> 'ArchiveDB':
         config = dotenv_values(".env")
         
         return ArchiveDB(
+            commit_dir = "./migrations",
             url = config["ARCHIVE_DB_URL"],
             experiments={}
         )
