570096a44f5043bcee5984f6e14d16b7104e184d
akina <renardakina@gmail.com>
2023-06-15 17:31:47 +0100

    cleaned

--- api/publication_api.py
+++ api/publication_api.py
@@ -17,21 +17,24 @@ from utils.logger import Logger
 
 @dataclass
 class PublicationAPI:
-    dat_provider : DatProvider
+    #dat_provider : DatProvider
     html_provider : HtmlProvider
     url : str
     api_key : str
     
+    """
+    send the publication data to the database
+        return :
+            None
+    """
     def send(self) :
-        data = self.merge()
-        try :    
-            print(data)
+        try :
+            data = self.merge()
             res = requests.post(
                 self.url,
                 json=data,
                 cookies={"access_token":self.api_key}
-            )    
-            
+            )                
             if res.status_code == 409:
                 response = json.loads(res.text)
                 if "requested_id" in response:
@@ -45,32 +48,30 @@ class PublicationAPI:
     def merge(self) -> dict:
         default_tags= {}
         tags = ["title", "authors_short", "authors_full", "journal", "year", "volume", "pages",\
-             "doi", "owner_name", "owner_email", "abstract", "brief_desc", "expts_paper", "expts_web"]
-        
-        dat_data = self.dat_provider.parse(default_tags,tags)
+             "doi", "owner_name", "owner_email", "abstract", "brief_desc", "expts_web"]
         data = {}
+        
+        """
+        dat_data = self.dat_provider.parse(default_tags,tags)
         #clean dat data
         for file in dat_data:
             data[file] = {}
             for key,value in dat_data[file].items():
                 if value is not None and value != "" and value != []:
                     data[file][key] = value
-        #print("DAT :")
-        #print(dat_data)
+        """
         
-        #si 3eme colonne
-
         html_data = self.html_provider.parse(default_tags,tags)
         #clean html data
         for file in html_data:
-            if not file in data.keys():
-                data[file] = {}
+            data[file] = {}
             for key,value in html_data[file].items():
+                if key == "expts_web" and len(value) == 0:
+                    raise Exception(f"no experiment ids have been parsed for file {file}")
                 if value is not None and value != "" and value != []:
                     data[file][key] = value
         
         data = list(data.values())
-        
         #grep all exp ids
         _exp_ids = [publication["expts_web"] for publication in data]
         exp_ids = []
@@ -85,8 +86,8 @@ class PublicationAPI:
 
         return data
         
+
     def notify(self,requested_exp_ids:list) :
-        
         f = open("request_ids.json", "w+")
         s = f.read()
         content = json.loads(s) if s != "" else {}
@@ -98,16 +99,23 @@ class PublicationAPI:
         Logger.console().warning(f"requested ids are : {requested_exp_ids}")
      
         
-    
+    """
+    initialize the providers, given the nimbus parameter
+        param :
+            filepath : str
+        return :
+            PublicationAPI
+    """
     @staticmethod
     def build(filepath : str) -> 'PublicationAPI':
         if not path.exists(filepath):
-            dat_provider = DatProvider([])
+            #dat_provider = DatProvider([])
             html_provider, ok = HtmlProvider.build_from_src(filepath)
             if not ok :
-                raise Exception(f"{filepath} does not exist or could not be accessed")
+                Logger.console().warning(f"{filepath} does not exist or could not be accessed")
+                return None
         else :
-            dat_provider = DatProvider.build(filepath)
+            #dat_provider = DatProvider.build(filepath)
             html_provider = HtmlProvider.build(filepath)
 
         route = "insert/publication"
@@ -116,7 +124,7 @@ class PublicationAPI:
         api_key = config['API_KEY']
         
         return PublicationAPI(
-            dat_provider = dat_provider,
+            #dat_provider = dat_provider,
             html_provider = html_provider,
             url = url,
             api_key = api_key,
